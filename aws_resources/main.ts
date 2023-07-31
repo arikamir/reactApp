c5648bc3c7bfc45c90f5aa89e73a3a018f5a7a87
Arik Amir <aamir@liveperson.com>
2023-07-31 14:56:21 +0300

    const aws = new provider.AwsProvider

--- aws_resources/main.ts
+++ aws_resources/main.ts
@@ -2,11 +2,14 @@ import { Construct } from 'constructs';
 import { App, TerraformStack, TerraformOutput as Output } from 'cdktf';
 
 import { S3Bucket } from './.gen/providers/aws/s3-bucket';
+import { provider } from '@cdktf/provider-aws';
 
 class StaticWebsiteStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
-
+      const aws = new provider.AwsProvider(this, 'aws', {
+      region: 'us-east-1',
+    });
      // Create an S3 bucket for the static website with website configuration
      const websiteBucket = new S3Bucket(this, 'websiteBucket', {
       bucket: 'aws-reactapp', // Change this to a unique bucket name for your website
