c553cf5bb0a96e94ef31aa4a0d8f198ba89154c0
Arik Amir <aamir@liveperson.com>
2023-07-31 14:29:22 +0300

    const websiteBucket = new S3Bucket(this, 'websiteBucket', {

--- aws_resources/main.ts
+++ aws_resources/main.ts
@@ -1,37 +1,34 @@
 import { Construct } from 'constructs';
-import { App, TerraformStack } from 'cdktf';
-import { provider, s3Bucket } from './.gen/providers/aws';
+import { App, TerraformStack, TerraformOutput as Output } from 'cdktf';
+
+import { S3Bucket } from './.gen/providers/aws/s3-bucket';
 
 class StaticWebsiteStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // Initialize the AWS provider
-    new provider(this, 'aws', {
-      region: 'us-east-1', // Change this to your desired region
+     // Create an S3 bucket for the static website with website configuration
+     const websiteBucket = new S3Bucket(this, 'websiteBucket', {
+      bucket: 'your-unique-bucket-name', // Change this to a unique bucket name for your website
+      acl: 'public-read',
+      website: [{
+        indexDocument: 'index.html',
+        errorDocument: '404.html',
+      }],
     });
 
-    // Create an S3 bucket for the static website
-    const websiteBucket = new s3Bucket(this, 'websiteBucket', {
-      bucket: 'aws-reactapp', // Change this to a unique bucket name for your website
-      acl: 'public-read',
-      website: [
-        {
-          indexDocument: 'index.html', // Change this to the name of your index file
-          errorDocument: 'error.html', // Change this to the name of your error file
-        },
-      ],
-    });
 
     // Output the bucket URL
     new Output(this, 'websiteUrl', {
-      value: websiteBucket.websiteEndpoint,
+      value: websiteBucket.websiteEndpoint!,
     });
   }
 }
 
-const app = new App();
 
 // Create a new CDKTF stack
+new StaticWebsiteStack(app, 'StaticWebsiteStack');
+
+const app = new App();
 new StaticWebsiteStack(app, 'StaticWebsiteStack');
 app.synth();
